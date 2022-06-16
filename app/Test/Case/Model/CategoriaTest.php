c7b55d79c2c8386ec60ce80c8f81c4011ccea404
Joao Bittencourt <joaogsbittencourt@gmail.com>
2022-06-16 20:14:15 -0300

    em desenv.

--- app/Test/Case/Model/CategoriaTest.php
+++ app/Test/Case/Model/CategoriaTest.php
@@ -6,13 +6,22 @@ class CategoriaTest extends CakeTestCase {
 
     public function setUp() {
         parent::setUp();
-        $this->Categorium = ClassRegistry::init('Categoria');
+        $this->Categoria = ClassRegistry::init('Categoria');
     }
 
     public function tearDown() {
-        unset($this->Categorium);
+        unset($this->Categoria);
 
         parent::tearDown();
+    }
+
+    public function testFindAllOrconditionsWhitoutConditions() {
+        $this->markTestIncomplete('testAdd not implemented.');
+//        $results = $this->Categoria->findAllOrconditions();
+//        
+//        $countResults = (int) count($results);
+//        
+//        $this->assertEquals(2, $countResults);
     }
 
 }
